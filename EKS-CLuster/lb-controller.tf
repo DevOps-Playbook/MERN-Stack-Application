9efd24fc3152544fad8a3cc4f513ab8a83dd71d7
Nishant Nath <108796288+amigo-nishant@users.noreply.github.com>
2025-12-22 11:13:01 +0530

    Create lb-controller.tf

---
+++ EKS-CLuster/lb-controller.tf
@@ -0,0 +1,106 @@
+provider "tls" {}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+    }
+  }
+}
+
+data "http" "lb_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+# Get OIDC Thumbprint for IAM
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_policy" "lb_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy_ALB"
+  description = "Policy for AWS Load Balancer Controller"
+  policy      = data.http.lb_policy.response_body
+}
+
+resource "aws_iam_role" "lb_controller" {
+  name = "AmazonEKSLoadBalancerControllerRole_ALB"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+            "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lb_controller_attach" {
+  role       = aws_iam_role.lb_controller.name
+  policy_arn = aws_iam_policy.lb_controller.arn
+}
+
+
+resource "helm_release" "lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  timeout    = 900 # 15 minutes timeout
+
+  set = [
+    {
+      name  = "clusterName"
+      value = aws_eks_cluster.eks_cluster.name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.lb_controller.arn
+    },
+
+    {
+      name  = "vpcId"
+      value = module.networking.vpc_id
+    },
+    {
+      name  = "region"
+      value = var.aws_region
+    }
+
+  ]
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster,
+    aws_iam_role_policy_attachment.lb_controller_attach
+  ]
+}
